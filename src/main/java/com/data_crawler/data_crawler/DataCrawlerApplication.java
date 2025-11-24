9ae57a02d4cec0d54cba2f0b65ea6f63c1b12091
hazelemma703 <hazelemma703@gmail.com>
2025-11-23 20:56:33 -0400

    feat: first setup

---
+++ src/main/java/com/data_crawler/data_crawler/DataCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.data_crawler.data_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data_crawler/data_crawler/DataCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_crawler.data_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
